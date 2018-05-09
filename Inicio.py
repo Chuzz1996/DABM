30f20c8b920bd1ea8d29c3e18acb04a75568dcb2
Carlos Alberto Ramirez Otero <carlosramirez-2997@hotmail.com>
2018-05-08 22:45:52 -0500

    Final v1

--- Inicio.py
+++ Inicio.py
@@ -11,6 +11,7 @@ from Arduino import *
 from PyQt5.QtWidgets import QWidget,QMessageBox,QApplication
 from PyQt5 import QtCore, QtGui, QtWidgets
 import threading
+import time
 from speech import *
 
 class Ui_MainWindow(object):
@@ -49,9 +50,11 @@ class Ui_MainWindow(object):
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(20, 310, 91, 101))
         self.label_3.setObjectName("label_3")
+        self.label_3.setStyleSheet("border-image: url(ui/luzEner.jpg);")
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
         self.label_4.setGeometry(QtCore.QRect(20, 420, 91, 101))
         self.label_4.setObjectName("label_4")
+        self.label_4.setStyleSheet("border-image: url(ui/ventEner.jpg);")
         self.label_5 = QtWidgets.QLabel(self.centralwidget)
         self.label_5.setGeometry(QtCore.QRect(130, 350, 101, 31))
         font = QtGui.QFont()
@@ -110,6 +113,7 @@ class Ui_MainWindow(object):
         self.labelLucesHab2.setText("")
         self.labelLucesHab1.setStyleSheet("border-image: url(ui/bombApagado.jpg);")
         self.labelLucesHab2.setObjectName("labelLucesHab2")
+        self.labelLucesHab2.setStyleSheet("border-image: url(ui/fueraServ.jpg);")
         self.labelVentiladorHab1 = QtWidgets.QLabel(self.centralwidget)
         self.labelVentiladorHab1.setGeometry(QtCore.QRect(290, 380, 191, 151))
         self.labelVentiladorHab1.setText("")
@@ -119,6 +123,7 @@ class Ui_MainWindow(object):
         self.labelVentiladorHab2.setGeometry(QtCore.QRect(570, 380, 191, 151))
         self.labelVentiladorHab2.setText("")
         self.labelVentiladorHab2.setObjectName("labelVentiladorHab2")
+        self.labelVentiladorHab2.setStyleSheet("border-image: url(ui/fueraServ.jpg);")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
@@ -153,12 +158,17 @@ class Ui_MainWindow(object):
             Speech("No esta conectado el Arduino").speech()
         
     def _ReconocerVoz(self):
-        VoiceThread(voiceRecognition(),self).start()
+        self._ConexionArduino()
+        if(self.arduinoDisponible==True and self.arduino.RevisarConexionArduino()==True):
+            VoiceThread(voiceRecognition(),self).start()
+        else:
+            Speech("No esta conectado el Arduino").speech()
 
     def accion(self):
         self._ConexionArduino()
         if(self.arduinoDisponible==True and self.arduino.RevisarConexionArduino()==True):
-            self.graphicsView.setStyleSheet("border-image: url(ui/recVoz.jpg);")
+            imagevoicethread = VoiceRecordImage(self.graphicsView)
+            imagevoicethread.start()
             indice = self.dictionary.containESdictionary(self.word)
             if(indice >= 0):
                 if(indice <= 1):
@@ -185,8 +195,8 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.btnReconocerVoz.setText(_translate("MainWindow", "Reconocer Voz"))
-        self.label_3.setText(_translate("MainWindow", "TextLabel"))
-        self.label_4.setText(_translate("MainWindow", "TextLabel"))
+        self.label_3.setText(_translate("MainWindow", ""))
+        self.label_4.setText(_translate("MainWindow", ""))
         self.label_5.setText(_translate("MainWindow", "Luces"))
         self.label_6.setText(_translate("MainWindow", "Ventilador"))
         self.label_7.setText(_translate("MainWindow", "Primer Piso"))
@@ -196,8 +206,19 @@ class Ui_MainWindow(object):
         self.label_11.setText(_translate("MainWindow", "Ventilador Habitación 2"))
 
 
+class VoiceRecordImage(threading.Thread):
+    def __init__(self, graphicsView):
+        threading.Thread.__init__(self)
+        self.graphicsView = graphicsView
+
+    def run(self):
+        self.graphicsView.setStyleSheet("border-image: url(ui/recVoz.jpg);")
+        time.sleep(3)
+        self.graphicsView.setStyleSheet("color: rgb(255, 255, 255);")
+
+        
+
 class VoiceThread(threading.Thread):
-    
     def __init__(self,voice,grafig):
         threading.Thread.__init__(self)
         self.voice = voice
@@ -210,5 +231,6 @@ class VoiceThread(threading.Thread):
                 self.grafig.setWord("Acabo")
             else:
                 self.grafig.setWord(self.valor)
+            Speech(self.valor).speech()
         except:
             Speech("we cant lisent you").speech()
--- voiceRecognition.py
+++ voiceRecognition.py
@@ -1,28 +1,19 @@
 import string
+import random
 import speech_recognition as sr
-from threading import Thread
-import socket
 
-class voiceRecognition(Thread):
+class voiceRecognition:
 
     def __init__(self):
-        Thread.__init__(self)
-        self.s = socket.socket()
-        self.s.connect(("localhost",6000))
         self.r = sr.Recognizer()
 
     def captureVoice(self):
         with sr.Microphone() as source:
             print("Say somethig: ")
             audio = self.r.listen(source)
-            self.s.send(self.r.recognize_google(audio).encode("utf-8"))
-            inputdata = self.s.recv(1024)
-            print(inputdata)
+            return self.r.recognize_google(audio)
 
-    def run():
-        self.captureVoice()
+    '''def captureVoice(self):
+        a = ["on","off","1","2"]
+        return a[random.randint(-1, 3)]'''
         
-def main():
-    x = voiceRecognition()
-    x.start()
-main()
